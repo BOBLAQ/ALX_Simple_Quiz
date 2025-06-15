323647e9baa3279482084edf8fe75f50cc16218b
BOBLAQ <ifeanyiben006@gmail.com>
2025-06-15 16:23:40 +0100

    Update checkAnswer.js

--- checkAnswer.js
+++ checkAnswer.js
@@ -1,5 +1,13 @@
 function checkAnswer() {
     // Function body
     correctAnswer="4"
+    <form>
+  <label><input type="radio" name="quiz" value="2">2</label>
+  <label><input type="radio" name="quiz" value="3">3</label>
+  <label><input type="radio" name="quiz" value="4">4</label>
+  <label><input type="radio" name="quiz" value="5">5</label>
+</form>
+<div id="feedback"></div>
+<button id="submit-answer">Submit Answer</button>
 }
 
